b8d1dac9e32dfbe4b0935819cf85f2cd7657198b
jmatuszak <janek.matuszak95@live.com>
2023-04-08 00:50:03 +0200

    template- add.remove excercise

--- GymLog/Controllers/TemplateController.cs
+++ GymLog/Controllers/TemplateController.cs
@@ -21,9 +21,6 @@ namespace GymLog.Controllers
 		}
 
 
-
-
-
         public async Task<List<CreateExcerciseConcatVM>> CreateExcerciseConcatList()
         {
             var excercises = await _context.Excercises.ToListAsync();
@@ -39,11 +36,16 @@ namespace GymLog.Controllers
             }
             return excercisesVM;
         }
-        public ExcerciseSetVM AddSet(ExcerciseSetVM ExcerciseSetVM)
+
+        //<-----------------------   Set   ---------------------> 
+        public TemplateVM AddSet(TemplateVM templateVM, int i)
         {
-            if (ExcerciseSetVM.SetsVM == null) ExcerciseSetVM.SetsVM = new List<SetVM>();
-            ExcerciseSetVM.SetsVM.Add(new SetVM());
-            return ExcerciseSetVM;
+            if (templateVM == null) throw new ArgumentNullException();
+            if (templateVM.ExcerciseSetsVM == null) throw new ArgumentNullException();
+            if (templateVM.ExcerciseSetsVM[i].SetsVM == null) 
+                templateVM.ExcerciseSetsVM[i].SetsVM = new List<SetVM>();
+            templateVM.ExcerciseSetsVM[i].SetsVM.Add(new SetVM());
+            return templateVM;
 
         }
         public ExcerciseSetVM RemoveSet(ExcerciseSetVM ExcerciseSetVM)
@@ -53,18 +55,29 @@ namespace GymLog.Controllers
             return ExcerciseSetVM;
         }
 
+
+        public IActionResult AddSetCreate(int id,TemplateVM templateVM)
+        {
+            templateVM = AddSet(templateVM,id);
+            return View("Create", templateVM);
+        }
+
+
+
+        //<-----------------------   Excercise   ---------------------> 
+
         public TemplateVM AddExcerciseSet(TemplateVM templateVM)
         {
             if (templateVM.ExcerciseSetsVM == null) templateVM.ExcerciseSetsVM = new List<ExcerciseSetVM>();
             templateVM.ExcerciseSetsVM.Add(new ExcerciseSetVM());
             return templateVM;
-
         }
-
-
-
-        //<-----------------------  CREATE   --------------------->   
-
+        public TemplateVM RemoveExcerciseSet(TemplateVM templateVM)
+        {
+            if (templateVM.ExcerciseSetsVM == null) templateVM.ExcerciseSetsVM = new List<ExcerciseSetVM>();
+            templateVM.ExcerciseSetsVM.RemoveAt(templateVM.ExcerciseSetsVM.Count-1);
+            return templateVM;
+        }
 
         public IActionResult AddExcerciseSetCreate(TemplateVM templateVM)
         {
@@ -73,13 +86,15 @@ namespace GymLog.Controllers
             return View("Create", templateVM);
 
         }
-        public IActionResult RemoveExcerciseSetCreate(ExcerciseSetVM ExcerciseSetVM)
+        public IActionResult RemoveExcerciseSetCreate(TemplateVM templateVM)
         {
-            if (ExcerciseSetVM.SetsVM == null) return View("Error");
-            ExcerciseSetVM = RemoveSet(ExcerciseSetVM);
-            return View("Create", ExcerciseSetVM);
+            if (templateVM.ExcerciseSetsVM == null) return View("Error");
+            templateVM = RemoveExcerciseSet(templateVM);
+            return View("Create", templateVM);
         }
 
+
+        //<-----------------------   Create   ---------------------> 
         public async Task<IActionResult> Create(TemplateVM? templateVM)
         {
             //if (!ModelState.IsValid) return BadRequest(ModelState);
